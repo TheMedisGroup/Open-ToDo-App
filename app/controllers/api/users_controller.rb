0d0868900bb30d5dbc0195337de865fe6c0ad6c9
Michael Medis <TheMedisGroup@gmail.com>
2015-12-17 11:38:29 -0500

    api controllers updated

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::UsersController < ApiController
-
-    before_action :authenticated?
+  before_action :authenticated?
 
   def index
     @users = User.all
@@ -9,6 +8,21 @@ class Api::UsersController < ApiController
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
